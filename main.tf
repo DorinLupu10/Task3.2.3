ad6e456937f96c3bf76c4c38aa373f8fee6527c0
Dorin Lupu <dorin.lupu@outlook.com>
2026-05-20 16:55:39 +0300

    Add distribution

--- main.tf
+++ main.tf
@@ -55,4 +55,44 @@ resource "aws_cloudfront_origin_access_control" "website" {
   name                              = "dorin-static-website-oac"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# distribution
+resource "aws_cloudfront_distribution" "website" {
+  enabled             = true
+  default_root_object = "index.html"
+  aliases             = [var.domain_name, "www.${var.domain_name}"]
+
+  origin {
+    domain_name              = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id                = "S3Origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.website.id
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3Origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.website.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
 }
