e217561827cf96fdbe44b01974120ee0ce93bb2e
Dorin Lupu <dorin.lupu@outlook.com>
2026-05-20 16:18:57 +0300

    first commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "domain_name" {
+  description = "Domen"
+  default     = "wolflife.net"
+}
+
+variable "bucket_name" {
+  description = "Numele bucket"
+  default     = "dorin-s3bucket"
+}
