c50d36ea00b372601af8554a325eec21ecc2f036
Dorin Lupu <dorin.lupu@outlook.com>
2026-05-20 16:45:10 +0300

    Set bucket

---
+++ main.tf
@@ -0,0 +1,27 @@
+# S3 bbucket
+resource "aws_s3_bucket" "website" {
+  bucket = var.bucket_name
+}
+
+# Dezactiveaza public acces
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+#  bucket pentru website hosting
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
