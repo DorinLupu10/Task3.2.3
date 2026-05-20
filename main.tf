8ddd20c4f9a94ddee42ba490fb604e7e9ec97e32
Dorin Lupu <dorin.lupu@outlook.com>
2026-05-20 16:49:24 +0300

    Add bucket policy

--- main.tf
+++ main.tf
@@ -25,3 +25,28 @@ resource "aws_s3_bucket_website_configuration" "website" {
     key = "error.html"
   }
 }
+
+# Bucket policy  acces doar din Cloudfront
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontAccess"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.website.arn
+          }
+        }
+      }
+    ]
+  })
+}
